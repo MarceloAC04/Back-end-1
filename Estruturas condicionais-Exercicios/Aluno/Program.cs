e9e4d26f5b09cb9c60e81084414586ca9325f689
Marcelo <marcelocardoso020304@gmail.com>
2023-03-30 16:35:33 -0300

    ajustes nos exercicios

--- Estruturas condicionais-Exercicios/Aluno/Program.cs
+++ Estruturas condicionais-Exercicios/Aluno/Program.cs
@@ -1,18 +1,19 @@
 ﻿float frequencia;
+const float frequenciaMinima = 0.75f;
 float media;
 
 Console.WriteLine($"Informe a frequência do aluno");
 frequencia = float.Parse(Console.ReadLine());
 
-if (frequencia < 75)
+if (frequencia < frequenciaMinima)
 {
     Console.WriteLine($"Aluno reprovado");
 }
-else if (frequencia >= 75)
+else if (frequencia >= frequenciaMinima)
 {
     Console.WriteLine($"Informe a média do aluno");
     media = float.Parse(Console.ReadLine());
-    if (media >= 3 && media <= 7)
+    if (media > 3 && media < 7)
     {
         Console.WriteLine($"Aluno em recuperação");
     }
