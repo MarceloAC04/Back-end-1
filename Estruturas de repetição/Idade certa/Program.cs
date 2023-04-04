8efb7870d544cc3e08baf4b24b82eea97f1e42d5
Marcelo Araujo <marcelocardoso020304@gmail.com>
2023-04-04 10:53:36 -0300

    criada a pasta de repeticao

---
+++ Estruturas de repetição/Idade certa/Program.cs
@@ -0,0 +1,13 @@
+﻿bool idadeCerta = false;
+
+while (idadeCerta == false)
+{
+    Console.WriteLine($"Qual a idade do pericles ?");
+    int idade = int.Parse(Console.ReadLine());
+
+    if (idade == 53)
+    {
+     idadeCerta = true;
+    }
+    
+}
