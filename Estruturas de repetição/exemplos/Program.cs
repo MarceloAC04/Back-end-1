20a535c1c880581a0abfc30a25d39e5d80f4c97b
Marcelo Araujo <marcelocardoso020304@gmail.com>
2023-04-05 10:58:05 -0300

    feitos os exemplos de do while e for

--- Estruturas de repetição/exemplos/Program.cs
+++ Estruturas de repetição/exemplos/Program.cs
@@ -1,4 +1,6 @@
-﻿// bool idadeCerta = false;
+﻿//  WHILE \\
+
+// bool idadeCerta = false;
 
 // while (idadeCerta == false)
 // {
@@ -12,19 +14,51 @@
     
 // }
 
-Console.WriteLine($"Digite o seu nome");
-string nome = Console.ReadLine();
+// Console.WriteLine($"Digite o seu nome");
+// string nome = Console.ReadLine();
 
-Console.WriteLine($"Digite sua senha (tem que ter 6 caracteres)");
-string senha = Console.ReadLine();
+// Console.WriteLine($"Digite sua senha (tem que ter 6 caracteres)");
+// string senha = Console.ReadLine();
 
-while (senha.Length != 6)
+// while (senha.Length != 6)
+// {
+//     Console.WriteLine($"Digite a senha novamente no padrão requerido (com 6 caracteres)");
+//      senha = Console.ReadLine();
+// }
+
+// Console.WriteLine($"Usuário e senha recebidos com sucesso!");
+
+//------------------\\
+
+// DO WHILE \\
+
+// int i = 6;
+
+// do
+// {
+//  Console.WriteLine($"{i}");
+//  i++;
+// }
+// while (i < 5 );
+
+// bool idadeCerta = false;
+
+// do
+// {
+//     Console.WriteLine($"Qual a idade do pericles?");
+//     int idade = int.Parse(Console.ReadLine());
+//     if (idade == 53)
+//     {
+//         idadeCerta = true;
+//     }
+// }
+// while (idadeCerta == false);
+
+// FOR \\
+
+for (int t = 200; t >= 0; t-=2)
 {
-    Console.WriteLine($"Digite a senha novamente no padrão requerido (com 6 caracteres)");
-     senha = Console.ReadLine();
+    Console.WriteLine($"{t}");
+ 
 }
-
-Console.WriteLine($"Usuário e senha recebidos com sucesso!");
-
-
 
