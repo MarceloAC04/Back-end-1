226e89054a2ff1fca479d9b04bfc77f32ab9df1c
Marcelo Araujo <marcelocardoso020304@gmail.com>
2023-04-10 10:57:41 -0300

    criada a pasta de arrays

---
+++ Arrays/exemplos/ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿// string carro = "Opala";
+
+// // Console.WriteLine($"Cadastre o nome do carro");
+// // carro = Console.ReadLine();
+
+// Console.WriteLine($"O carro é {carro}");
+
+// carro = "fusca";
+
+// Console.WriteLine($"O carro é {carro}");
+
+
+string[] carros = new string[3];
+carros[0] = "Tesla";
+carros[1] = "Maverick";
+carros[2] = "Celta 94";
+
+// carros[3] = "ferrari";
+
+Console.WriteLine($"Os carros cadastrados são ({carros[0]}, {carros[1]}, {carros[2]})");
+
