113f7fcfea3eac1e220576390ffd7c3858decb8a
Marcelo Araujo <marcelocardoso020304@gmail.com>
2023-04-12 10:57:34 -0300

    criado a pastad de funções

---
+++ Funções/exemplos/ex1/Program.cs
@@ -0,0 +1,50 @@
+﻿// static void Saudacao()
+// {
+//     Console.WriteLine($"Informe o nome da pessoa");
+//     string nome = Console.ReadLine();
+//     Console.WriteLine($"Informe o sobrenome da pessoa");
+//     string sobrenome = Console.ReadLine();
+//     Console.WriteLine($"Bom dia! {nome} {sobrenome}");
+    
+// }
+
+// Saudacao();
+
+static float Somar(float n1, float n2)
+{
+    return (n1 + n2);
+}
+static float Subtrair(float n1, float n2)
+{
+    return (n1 - n2);
+}
+static float Dividir(float n1, float n2)
+{
+    return (n1 / n2);
+}
+static float Multiplicar(float n1, float n2)
+{
+    return (n1 * n2);
+}
+
+Console.WriteLine($"Informe o primeiro numero");
+float n1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o segundo numero");
+float n2 = float.Parse(Console.ReadLine());
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine($"A soma de {n1} + {n2} é {Somar(n1,n2)} ");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine($"A subtração de {n1} - {n2} é {Subtrair(n1,n2)} ");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine($"A divisão de {n1} / {n2} é {Dividir(n1,n2)} ");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine($"A multiplicação de {n1} x {n2} é {Multiplicar(n1,n2)} ");
+Console.ResetColor();
---
+++ Funções/exemplos/ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿static void BarraCarregamento(string texto, int pontos, int tempo)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write(texto);
+
+    for (int i = 0; i < pontos; i++)
+    {
+        Console.Write($".");
+        Thread.Sleep(tempo);
+    }
+    Console.ResetColor();
+    Console.WriteLine($"");
+}
+
+BarraCarregamento("Testando",10,700);
+
+BarraCarregamento("Carregando",15,1000);
+
+BarraCarregamento("Finalizando",8,700);
+
+
+
